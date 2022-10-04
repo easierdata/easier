4b8010d2cb353bfecb456a0c3d1d7d77b14cc0d2
John Solly <jsolly@protonmail.com>
2022-10-03 17:37:23 -0700

    Add shell scripts

---
+++ code/shellScripts/chirps_extract.sh
@@ -0,0 +1,9 @@
+# chirps 
+#tifs/netcdf/bils/cogs
+output_dir=""
+
+# wget -m -e robots=off -nH --cut-dirs=2 --reject-regex '\?' -R .html,.tmp,.DS_Store -np -r https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_monthly/tifs/ -P $output_dir
+# wget -m -e robots=off -nH --cut-dirs=2 --reject-regex '\?' -R .html,.tmp,.DS_Store -np -r https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_monthly/netcdf/ -P $output_dir
+# wget -m -e robots=off -nH --cut-dirs=2 --reject-regex '\?' -R .html,.tmp,.DS_Store -np -r https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_monthly/bils/ -P $output_dir
+# wget -m -e robots=off -nH --cut-dirs=2 --reject-regex '\?' -R .html,.tmp,.DS_Store -np -r https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_monthly/cogs/ -P $output_dir
+wget -m -e robots=off -nH --cut-dirs=2 --reject-regex '\?' -R .html,.tmp,.DS_Store -np -r https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/netcdf/ -P $output_dir
---
+++ code/shellScripts/data_aquisition.sh
@@ -0,0 +1,13 @@
+# token= .env
+year="2015"
+output_dir=""
+dataset="MOD09Q1G_NDVI"
+#dataset="MOD09"
+#echo "dry run $token"
+for i in {1..361..8}
+#for i in {1..366} 
+do
+    target=$(printf %03d $i)
+    echo "$target"
+    wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/6/$dataset/$year/$target/" --header "Authorization: Bearer $token" -P "$output_dir"
+done
