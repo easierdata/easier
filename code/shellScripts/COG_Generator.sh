6336f3e7dd157eebbafc21649a2b668f6fed02eb
Zheng Liu <liuz@gsapp6.gshpc.umd.edu>
2022-10-24 11:35:21 -0400

    new findings on the TILED parameter

--- code/shellScripts/COG_Generator.sh
+++ code/shellScripts/COG_Generator.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-module load gdal #for cluster usage
-FILES="../../data/input/*.TIF"
+module load gdal/3.1.0 #for cluster usage
+FILES="./data/input/*.TIF"
 for f in $FILES
 do
   echo "Processing $f"
   gdal_translate $f "${f%.*}".cog -of COG -co COMPRESS=LZW
+  #new finding: After GDAL/3.1.0, there is no TILED parameter and it is automatically included
+  #source: https://gdal.org/drivers/raster/cog.html
 done
