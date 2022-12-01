76f168c2dc081dbd9b7b0dac0579bb8fa44dd6ad
John Solly <jsolly@umd.edu>
2022-12-01 13:21:43 -0800

    Move bucket name into variable

--- code/shellScripts/egress_to_s3.sh
+++ code/shellScripts/egress_to_s3.sh
@@ -1,9 +1,10 @@
 startdate="20211107"
 enddate="20220501"
 datestr="$startdate"
+bucket="piknik:uofm-data1"
 module load rclone
 while [ "$(date -d "$datestr" +%Y%m%d)" -lt "$(date -d "$enddate" +%Y%m%d)" ]; do
     echo $datestr
-    rclone sync --checkers 50 --retries 6 --s3-upload-concurrency 32 --transfers 16 --s3-chunk-size 100M data/landsat/m2m_download/$datestr piknik:uofm-data1/landsat9-c2-l1/$datestr
+    rclone sync --checkers 50 --retries 6 --s3-upload-concurrency 32 --transfers 16 --s3-chunk-size 100M data/landsat/m2m_download/$datestr $bucket/landsat9-c2-l1/$datestr
     datestr=$(date +%Y%m%d -d "$datestr + 1 day" )
 done
