98661999f7dc23d44ad4ec5666d8b9472bdb8b1b
Zheng Liu <zliu1208@umd.edu>
2023-05-03 17:25:10 -0400

    egress code

--- code/shellScripts/egress_to_s3.sh
+++ code/shellScripts/egress_to_s3.sh
@@ -14,6 +14,7 @@ while [ "$(date -d "$datestr" +%Y%m%d)" -lt "$(date -d "$enddate" +%Y%m%d)" ]; d
     
     #Option 2: force update for bug files
     #rclone copy -v --ignore-existing --checkers 50 --retries 6 --s3-upload-concurrency 32 --transfers 16 --s3-chunk-size 100M data/landsat/m2m_download/$datestr piknik:uofm-data1/landsat9-c2-l1/$datestr
+    #rclone copy -v --ignore-existing --checkers 50 --retries 6 --s3-upload-concurrency 32 --transfers 16 --s3-chunk-size 100M data/landsat/landsat9-c2-l1-missing piknik:uofm-data1/landsat9-c2-l1-missing
     
 datestr=$(date +%Y%m%d -d "$datestr + 1 day" )
 done
