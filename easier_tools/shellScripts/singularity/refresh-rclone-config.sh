a9e9cb60b8cf6c9df1a30aac6e8c04986fd5e1d9
SethDocherty <sddocherty@gmail.com>
2024-11-08 15:56:46 -0500

    refresh of shell script based on the additions made to powershell version.

--- easier_tools/shellScripts/singularity/refresh-rclone-config.sh
+++ easier_tools/shellScripts/singularity/refresh-rclone-config.sh
@@ -1,70 +1,89 @@
 #!/bin/bash
 
-# Function to get a new token
-generate_token() {
-    # Check if ~/.urs_cookies file exists and delete if it does
-    if [ -f ~/.urs_cookies ]; then
-        rm ~/.urs_cookies
+# Function to check if the profile exists in the rclone config file
+function check_profile_exists {
+    profile_name=$1
+    rcloneConfigFile="$HOME/.config/rclone/rclone.conf"
+    if [ ! -f "$rcloneConfigFile" ]; then
+        echo "Error: rclone config file not found."
+        exit 1
     fi
-    # Call the Earthdata login endpoint to get a new token
-    response=$(curl -b ~/.urs_cookies -c ~/.urs_cookies -L -n https://data.ornldaac.earthdata.nasa.gov/s3credentials | jq -r '.')
-    # response=$(curl -b ~/.urs_cookies -c ~/.urs_cookies -L -n https://data.lpdaac.earthdatacloud.nasa.gov/s3credentials | jq -r '.')
-    # Replace the space in the expiration date with a 'T' to make it ISO 8601 compliant
-    expiration=$(echo "$response" | jq -r '.expiration' | sed 's/ /T/')
-    envString="ACCESS_KEY_ID=$(echo "$response" | jq -r '.accessKeyId')\nSECRET_ACCESS_KEY=$(echo "$response" | jq -r '.secretAccessKey')\nSESSION_TOKEN=$(echo "$response" | jq -r '.sessionToken')\nEXPIRATION=$expiration"
-    echo -e "$envString" > ~/.env
+    profileExists=$(grep -E "\[$profile_name\]" "$rcloneConfigFile")
+    if [ -z "$profileExists" ]; then
+        echo "Error: Profile '$profile_name' does not exist in the rclone config file."
+        echo "Valid profiles are:"
+        grep -E "^\[.*\]" "$rcloneConfigFile" | sed 's/^\[\(.*\)\]/\1/'
+        exit 1
+    fi
+}
+
+# Function to get a new token
+function generate_token {
+    profile_name=$1
+    if [[ $profile_name =~ "lp" ]]; then
+        daac_endpoint="data.lpdaac.earthdatacloud.nasa.gov"
+    elif [[ $profile_name =~ "ornl" ]]; then
+        daac_endpoint="data.ornldaac.earthdata.nasa.gov"
+    else
+        echo "Profile does not specify which DAAC to generate tokens. Check to see if the profile contains 'lp' or 'ornl'."
+        exit 1
+    fi
+    if [ -f "$HOME/.urs_cookies" ]; then
+        rm "$HOME/.urs_cookies"
+    fi
+    url="https://$daac_endpoint/s3credentials"
+    response=$(curl -b "$HOME/.urs_cookies" -c "$HOME/.urs_cookies" -L -n "$url")
+    if [ -z "$response" ]; then
+        echo "Failed to get a response from the server."
+        return
+    fi
+    expiration=$(echo "$response" | jq -r '.expiration')
+    if [ -z "$expiration" ]; then
+        echo "The response does not contain an expiration field."
+        return
+    fi
+    expiration=$(echo "$expiration" | sed 's/ /T/')
+    accessKeyId=$(echo "$response" | jq -r '.accessKeyId')
+    secretAccessKey=$(echo "$response" | jq -r '.secretAccessKey')
+    sessionToken=$(echo "$response" | jq -r '.sessionToken')
+    envString="ACCESS_KEY_ID=$accessKeyId\nSECRET_ACCESS_KEY=$secretAccessKey\nSESSION_TOKEN=$sessionToken\nEXPIRATION=$expiration"
+    echo -e "$envString" > "$HOME/.env"
 }
 
 # Update the rclone config file for a specific profile with the updated access key id, secret access key, and session token
 # The latest token details can be found in the .env file in the user's home directory
-update_rclone_config() {
-    profile=$1
-    envFile=~/.env
-    accessKeyId=$(grep -oP "(?<=ACCESS_KEY_ID=).*" "$envFile")
-    secretAccessKey=$(grep -oP "(?<=SECRET_ACCESS_KEY=).*" "$envFile")
-    sessionToken=$(grep -oP "(?<=SESSION_TOKEN=).*" "$envFile")
-    rcloneConfigFile=~/.config/rclone/rclone.conf
-    startLine=$(grep -n "\[$profile\]" "$rcloneConfigFile" | cut -d: -f1)
-    endLine=$(awk "/\[/{n++} n==$startLine" "$rcloneConfigFile")
-    endLine=$((endLine-1))
-
-    # Now you can use $startLine and $endLine to get the lines for the profile
-    profileLines=$(sed -n "$startLine,${endLine}p" "$rcloneConfigFile" | while IFS= read -r line; do
-        # Check if the current line contains the access key id, secret access key, or session token and update it
-        if echo "$line" | grep -q "access_key_id"; then
-            echo "access_key_id = $accessKeyId"
-        elif echo "$line" | grep -q "secret_access_key"; then
-            echo "secret_access_key = $secretAccessKey"
-        elif echo "$line" | grep -q "session_token"; then
-            echo "session_token = $sessionToken"
-        else
-            echo "$line"
-        fi
-    done)
-
-    # prepend the profile name to the beginning of $profileLines
-    # profileLines=$(echo "$profileLines" | sed "1s/^/[$profile]\n/")
-
-    # Get the lines before and after the profile but also account for when the profile is at the beginning
-    if [ "$startLine" -eq 1 ]; then
-        linesBeforeProfile=""
+function update_rclone_config {
+    profile_name=$1
+    if [[ $profile_name =~ "s3" ]]; then
+        generate_token "$profile_name"
     else
-        linesBeforeProfile=$(sed -n "1,$((startLine-1))p" "$rcloneConfigFile")
+        echo "Profile does not specify S3 storage. Tokens will not be generated."
+        return
     fi
-    linesAfterProfile=$(sed -n "$((endLine+2)),$ p" "$rcloneConfigFile")
-
-    echo "$profileLines"
-    echo "$startLine"
-    echo "$endLine"
-    # Combine the lines before the profile, the updated profile lines, and the lines after the profile
-    profileLines=$(echo -e "$linesBeforeProfile\n$profileLines\n$linesAfterProfile")
-
-    # Save the updated rclone config file
-    echo "$profileLines" > "$rcloneConfigFile"
-    echo "Updated rclone config file for profile: $profile"
+    envFile="$HOME/.env"
+    accessKeyId=$(grep -E "^ACCESS_KEY_ID=" "$envFile" | cut -d'=' -f2)
+    secretAccessKey=$(grep -E "^SECRET_ACCESS_KEY=" "$envFile" | cut -d'=' -f2)
+    sessionToken=$(grep -E "^SESSION_TOKEN=" "$envFile" | cut -d'=' -f2)
+    rcloneConfigFile="$HOME/.config/rclone/rclone.conf"
+    startLine=$(grep -nE "^\[$profile_name\]" "$rcloneConfigFile" | cut -d':' -f1)
+    endLine=$(grep -nE "^\[" "$rcloneConfigFile" | grep -A1 -m1 "^$startLine" | tail -n1 | cut -d':' -f1)
+    profileLines=$(sed -n "$startLine,$endLine p" "$rcloneConfigFile")
+    profileLines=$(echo "$profileLines" | sed -E "s/(access_key_id = ).*/\1$accessKeyId/")
+    profileLines=$(echo "$profileLines" | sed -E "s/(secret_access_key = ).*/\1$secretAccessKey/")
+    profileLines=$(echo "$profileLines" | sed -E "s/(session_token = ).*/\1$sessionToken/")
+    sed -i "$startLine,$endLine d" "$rcloneConfigFile"
+    sed -i "$startLine i$profileLines" "$rcloneConfigFile"
+    echo "Updated rclone config file for profile: $profile_name"
 }
 
+# Check if profile_name is provided
+if [ -z "$1" ]; then
+    echo "Error: You must provide a value for the profile_name parameter."
+    exit 1
+fi
+
+# Check if the profile exists in the rclone config file
+check_profile_exists "$1"
+
 # Example usage
-generate_token
-# update_rclone_config "earthdata-lp-s3"
-update_rclone_config "earthdata-ornl-s3"
+update_rclone_config "$1"
