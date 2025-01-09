1257f0539cbb16a6d024b2288230eb7a9e2d01df
Matthew Nanas <37747855+matthewnanas@users.noreply.github.com>
2025-01-09 15:57:04 -0500

    Fixes

--- easier_tools/shellScripts/get_airbyte.sh
+++ easier_tools/shellScripts/get_airbyte.sh
@@ -1,18 +1,18 @@
 # Get docker if it isn't already installed
 if ! command -v docker &> /dev/null; then
   echo "Docker is not installed. Installing Docker..."
-  sudo apt-get update
-  sudo apt-get install ca-certificates curl
+  sudo apt-get update -y
+  sudo apt-get install -y ca-certificates curl
   sudo install -m 0755 -d /etc/apt/keyrings
   sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   sudo chmod a+r /etc/apt/keyrings/docker.asc
 
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get update -y
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
   # Post install steps
-  sudo groupadd docker
+  sudo groupadd docker || true
   sudo usermod -aG docker $USER
   newgrp docker
 else
